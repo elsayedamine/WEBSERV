cda47201aba0b7df8b17d47e650b42c7f15f2b43
gnxrlyqf <gnarlydestructor@gmail.com>
2025-11-04 18:22:10 +0100

    parsed body

--- lhrba/request wip/req.cpp
+++ lhrba/request wip/req.cpp
@@ -27,16 +27,24 @@ string readline(int fd, bool includeDelimiter) {
 	ssize_t bytes_read;
 	
 	while ((bytes_read = read(fd, &c, 1)) > 0) {
-		if (c == '\r') {
+		if (c == '\0') {
+			// Null terminator - end of line
+			if (includeDelimiter) {
+				line += c;
+			}
+			break;
+		} else if (c == '\r') {
 			char next_c;
 			ssize_t next_read = read(fd, &next_c, 1);
 			if (next_read > 0 && next_c == '\n') {
+				// Found proper \r\n sequence
 				if (includeDelimiter) {
 					line += c;
 					line += next_c;
 				}
 				break;
 			} else {
+				// \r not followed by \n - continue reading
 				line += c;
 				if (next_read > 0) {
 					line += next_c;
@@ -73,18 +81,27 @@ pair<string, string> parseHeader(string line) {
 
 void parseRequest(int fd) {
 	string reqline;
-	string header;
+	string body;
+	string temp;
 	pair<string, string> en;
 	Request *request;
 	
 	reqline = readline(fd, false);
     request = parseRequestLine(reqline);
 	while (1) {
-		header = readline(fd, 0);
-		if (header.empty())
+		temp = readline(fd, 0);
+		if (temp.empty())
 			break;
-		en = parseHeader(header);
+		en = parseHeader(temp);
 		request->setHeader(en.first, en.second);
 	}
+	//readline(fd, 1);
+	while (1) {
+		temp = readline(fd, 1);
+		if (temp.empty())
+			break;
+		body += temp;
+	}
+	request->setBody(body);
 	cout << *request;
 }
